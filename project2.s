60b21a338b0d712a54af6aed07f399ca1d051d8a
Cflowe3129 <40706724+Cflowe3129@users.noreply.github.com>
2019-11-10 11:24:23 -0500

    exit
    I added an exit label and tabbed my previous code so that it's easier to read

--- project2.s
+++ project2.s
@@ -14,18 +14,26 @@
 
 main:
 
-la $s0, str #load value of string into register $s0
+    la $s0, str #load value of string into register $s0
 
-#READ STRING
-li $v0, 8 #read string
-la $a0, str #load address of string into register $a0
-li $a1, 11 #length of expected input including null char
-syscall
+    #READ STRING
+    li $v0, 8 #read string
+    la $a0, str #load address of string into register $a0
+    li $a1, 1000 #length of expected input including null char
+    syscall
 
-move $t0, $zero #initializes register $t0 as the index counter
+    move $t0, $zero #initializes register $t0 as the index counter
 
-lb $s2, 0($s0) #load char value in register $s0 and put it in $s2
+    lb $s2, 0($s0) #load char value in register $s0 and put it in $s2
 
-li $v0, 11 #print char value
-move $a0, $s2
+    li $v0, 11 #print char value
+    move $a0, $s2
+    syscall
+
+check_char:
+
+    
+exit:
+
+li $v0, 10
 syscall
