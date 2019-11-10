ea250dab9711b93448009584ce258790f116699d
Cflowe3129 <40706724+Cflowe3129@users.noreply.github.com>
2019-11-10 11:10:14 -0500

    Read string
    setting up command to read a string from user

--- project2.s
+++ project2.s
@@ -14,4 +14,11 @@
 
 main:
 
+la $s0, str #load value of string into register $s0
+
+#READ STRING
+li $v0, 8 #read string
+la $a0, str #load address of string into register $a0
+li $a1, 11 #length of expected input including null char
+syscall
 
