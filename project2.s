6f314700ce6da843b53e49411f17c1785ce0d793
Cflowe3129 <40706724+Cflowe3129@users.noreply.github.com>
2019-11-10 11:19:44 -0500

    print char
    print the first element in the stored string

--- project2.s
+++ project2.s
@@ -22,3 +22,10 @@ la $a0, str #load address of string into register $a0
 li $a1, 11 #length of expected input including null char
 syscall
 
+move $t0, $zero #initializes register $t0 as the index counter
+
+lb $s2, 0($s0) #load char value in register $s0 and put it in $s2
+
+li $v0, 11 #print char value
+move $a0, $s2
+syscall
