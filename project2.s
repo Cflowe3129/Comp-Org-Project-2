9a422ce64dd3754482980828e2916fe4955f35f4
Cflowe3129 <40706724+Cflowe3129@users.noreply.github.com>
2019-11-11 18:19:40 -0500

    exception error
    FIxing a qtspim unhandled exception error that I'm getting

--- project2.s
+++ project2.s
@@ -10,9 +10,9 @@
     
     invalid: .asciiz "Invalid input"
 
-    one: .word 27000
+#one: .word 27000
     
-    two: .word 900
+#two: .word 900
 
     three: .word 30
 
@@ -28,7 +28,7 @@ main:
     #READ STRING
     li $v0, 8 #read string
     la $a0, str #load address of string into register $a0
-    li $a1, 5 #length of expected input including null char
+    li $a1, 1000 #length of expected input including null char
     syscall
 
     move $t0, $zero #initializes register $t0 as the index counter
@@ -44,19 +44,22 @@ next_char:
 #   move $a0, $s2
 #   syscall
     
+    beq $s2, 9, print_invalid #checks if char is a tab character
+    beq $s2, 10, print_invalid #checks if char is a newline character
+    beq $s2, 32, print_invalid #checks if char is a space character
 
     bge $s2, 120, check_outside_base_1 #checks if char lies outside of base range
     bge $s2, 97, check_greater_lower #checks if char is >= a
     bge $s2, 88, check_outside_base_2 #checks if char lies outside of base range
     bge $s2, 65, check_greater_upper #checks if char is >= A
     bge $s2, 58, check_outside_base_3 #checks if char lies outside of base range
-    bge $s2, 48, check_greater_num #checks if char is >= 0
-    bge $s2, 0, check_outside_base_4 #checks if char lies outside of base range
+#bge $s2, 48, check_greater_num #checks if char is >= 0
+#bge $s2, 0, check_outside_base_4 #checks if char lies outside of base range
 
     
     addi $s0, $s0, 1 #increment index by 1
-    bge $t0, 6, print_invalid
-    ble $t0, 4, next_char #returns to loop1 label if counter in register $s0 != 10
+    bge $t0, 4, print_invalid
+    ble $t0, 3, next_char #returns to loop1 label if counter in register $s0 != 10
     j exit
 
 adjust_base:
